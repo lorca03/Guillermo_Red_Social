d9328ae5d98c5dde19002e8cb90d88fc0ad7f770
lorca03 <guillermolorcamartinez@gmail.com>
2023-01-28 18:59:03 +0100

    Boton comentar

--- Red_Social_Guillermo/resources/views/dashboard.blade.php
+++ Red_Social_Guillermo/resources/views/dashboard.blade.php
@@ -12,13 +12,13 @@
                     <labe>{{$image->user->name.'/@'.$image->user->user_name }}</labe>
                     <img style="width: 380px" src="{{'imagenes/'.$image->image_path}}" alt="No carga">
                     <labe>{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $image->created_at)->longRelativeDiffForHumans()}}</labe>
-                    <labe>Commentarios --> {{count($image->comments)}} <button type="submit">Ver todos</button> </labe>
+                    <labe>Commentarios --> {{count($image->comments)}} <button class="text-indigo-500 hover:underline" type="submit">Ver todos</button> </labe>
                     <br>
-                    <form method="POST" action="{{ route('save.comentario') }}">
+                    <form method="POST" class="flex" action="{{ route('save.comentario') }}">
                         @csrf
                         <textarea placeholder="Que piensas?" name="comentario"  cols="25" rows="1"></textarea>
                         <input type="hidden" value="{{$image->id}}" name="imagenCommentario">
-                        <input class="bg-indigo-500 rounded-lg p-2 text-white" type="submit" value="Comentar">
+                        <input class="bg-indigo-500 rounded-lg p-2 ml-1 text-white" type="submit" value="Comentar">
                     </form>
                     <br>
                 @endforeach
